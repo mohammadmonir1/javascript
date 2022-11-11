db80daa044f311968ac4b90bbca07ff3963045ac
mohammadmonir1 <mdmonir103912@gmail.com>
2022-11-11 16:07:45 +0600

    javascript

--- Basic.js
+++ Basic.js
@@ -40,4 +40,34 @@ var number = 0.1;
 var number1 = 0.2;
 var total = number + number1;
 total = total.toFixed(2);
-console.log(total)
+// console.log(total)
+
+var price = 30;
+var price1 = 40
+var total = price + price1;
+// console.log(total)
+// aikhane jog biyog gun vag kora jabe
+
+var price = 30;
+var price1 = 40
+// price = price + 2
+price++
+var total = price + price1;
+// console.log(total)
+
+var ami = "mohammad";
+var ami1 = "Monir"
+var total = ami + " " + ami1;
+// console.log(total);
+
+var num = 3.2345;
+// var Number = Math.abs(num);
+// var Number = Math.round(num);
+// var Number = Math.floor(num);
+// var Number = Math.ceil(num);
+// console.log(Number)
+
+var number = Math.random() * 5;
+// var result = number;
+var result = Math.round(number);
+console.log(result);

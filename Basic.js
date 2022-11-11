986caed074106ceefbc3768a596b55a2253c2653
mohammadmonir1 <mdmonir103912@gmail.com>
2022-11-11 15:49:02 +0600

    javascript

---
+++ Basic.js
@@ -0,0 +1,43 @@
+var mango = 14;
+// console.log(typeof mango);
+
+var ami = "monir";
+// console.log(typeof ami);
+
+var isHot = true;
+// console.log(isHot);
+
+var isRich = false;
+// console.log(isRich)
+
+var promise = "I never go alone without you";
+// console.log(promise.toUpperCase());
+// console.log(promise.toLocaleLowerCase());
+// console.log(promise.indexOf("go"));
+// console.log(promise.split("I"));
+
+// JavaScript Jog
+var number = 25;
+var number1 = 15.5;
+// console.log(number + number1);
+
+var number = 25;
+var number1 = "15.5";
+number1 = parseFloat(number1);
+// console.log(number + number1)
+
+var number = 25;
+var number1 = "15.5";
+number1 = parseInt(number1);
+// console.log(number + number1)
+
+var number1 = 15;
+var number2 = 15.5;
+number1 = "" + number1;
+// console.log(number1);
+
+var number = 0.1;
+var number1 = 0.2;
+var total = number + number1;
+total = total.toFixed(2);
+console.log(total)

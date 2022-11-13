d7302a8453711fca48ef6b242bdac6cab8f50ae8
mohammadmonir1 <mdmonir103912@gmail.com>
2022-11-13 14:59:16 +0600

    javascript

--- loopAndProblemSolve.js
+++ loopAndProblemSolve.js
@@ -97,4 +97,116 @@ student2["Movie"] = "Ertugrul";
 // console.log(student2);
 var student2Movie = "Ertugrul";
 var student2 = student2 [student2Movie];
-console.log(student2);
+// console.log(student2);
+
+// inch to feet
+
+function inchToFeet(inch){
+    var feet = inch / 12;
+    return feet;
+}
+var result = inchToFeet(144);
+// console.log(result)
+
+var senior = [156, 288, 300];
+for (var i = 0; i < senior.length; i++){
+    var sum = senior[i];
+    // console.log(sum / 12)
+}
+
+// About Let and Const
+// let = jodi kono varriable er man poriborton hoy taholse sekhane let hobe,,,
+// const = jodi kono varriable er man poriborton na hoy taholse sekhane let hobe,,,
+
+// Leap year
+
+// const year = 2027;
+// let reminder = year / 4;
+
+//  if(reminder = 0){
+    // console.log("You are a leap year");
+    
+// }
+// else{
+    // console.log("You are not leap year")
+//  }
+
+// const year = 2024;
+// const reminder = (year % 100 === 0) ? (year % 400 === 0): (year % 4 === 0);
+
+// if(reminder){
+    // console.log("This is Leap Year");
+// }
+// else{
+    // console.log("This is not leap year")
+// }
+
+// functio use kore
+function isLeapYear(year){
+    const reminder = (year % 100 === 0) ? (year % 400 === 0): (year % 4 === 0);
+    if (reminder){
+        return true;
+    }else{
+        return false
+    }
+
+}
+var result = isLeapYear(2024);
+// console.log(result)
+
+// Factorial
+// emample = 1*1*2*3*4*5*6*7*8*9*10
+// var factorial= 1;
+// for (var i = 1; i <= 10; i++){
+    // factorial = factorial* i;
+    // console.log(i, factorial)
+// }
+// use function
+function factorial (n){
+    var factorial = 1;
+    for (var i = 1; i <= n; i++){
+        factorial = factorial * i;
+    }
+    return factorial;
+}
+var result = factorial(10);
+// console.log(result)
+
+
+// while loop
+// var i = 1;
+// var factorial = 1;
+// while (i<=10){
+//     factorial = factorial * i;
+//     // console.log(i, factorial)
+//     i++;
+// }
+// console.log(factorial)
+
+// while loop with function
+
+function factorial(n){
+    var i = 1;
+    var factorial = 1;
+while (i <= n){
+    factorial = factorial * i;
+    // console.log(i, factorial)
+    i++;
+    }
+    return factorial
+}
+var result = factorial(10);
+// console.log(result)
+
+
+// factorial wih recursive
+
+function factorial(n){
+    if ( n == 0){
+        return 1;
+    }else{
+        return n * factorial(n-1);
+    }
+}
+var result = factorial(10);
+// console.log(result)
